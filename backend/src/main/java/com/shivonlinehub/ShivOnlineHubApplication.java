ddcaee1fc527218df298d343e4f1bc997b1f65cc
LingannaJ <jlinganna104@gmail.com>
2025-04-27 16:25:55 +0530

    shiv-online

---
+++ backend/src/main/java/com/shivonlinehub/ShivOnlineHubApplication.java
@@ -0,0 +1,11 @@
+package com.shivonlinehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShivOnlineHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShivOnlineHubApplication.class, args);
+    }
+}
